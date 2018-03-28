849064a5dec0b9a74b12a4a28b6a2877ff74df26
Mikhail Chatillon <mikhail.chatillon@hotmail.com>
2018-03-28 15:04:59 +0200

    init commit

---
+++ Assets/DataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    class DataSource
+    {
+        public Dictionary<string, List<double>> dataSet;
+        public DataSource()
+        {
+            dataSet = new Dictionary<string, List<double>>();
+            var rndSet = new System.Random(DateTime.Now.Millisecond);
+            var i = 0;
+            while (i < rndSet.Next(5, 10))
+            {
+                var rndBar = new System.Random(DateTime.Now.Millisecond);
+                var vals = new List<double>();
+                for (var b = 0; b <= 11; b++)
+                {
+                    vals.Add(rndBar.NextDouble() * (150 - 2) + 2);
+                }
+                dataSet.Add(string.Format("category {0}", i), vals);
+                i++;
+            }
+        }
+    }
+}
---
+++ Assets/GraphController.cs
@@ -0,0 +1,50 @@
+﻿using Assets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphController : MonoBehaviour {
+
+    public GameObject barPrefab;
+
+    public List<GameObject> bars;
+    private int categoriesValues;
+    private DataSource generator;
+
+    // Use this for initialization
+    void Start()
+    {
+        generator = new DataSource();
+       
+
+        Vector3 currentOffset = Vector3.zero;
+
+        foreach (var key in generator.dataSet.Keys)
+        {
+            foreach (var value in generator.dataSet[key])
+            {
+                GameObject go = Instantiate(barPrefab);
+                go.transform.localScale = new Vector3(1, (float)value, 0.1f);
+                go.transform.position = new Vector3(currentOffset.x, go.transform.localScale.y / 2, currentOffset.z);
+                currentOffset.x += 2;
+                go.transform.parent = this.transform;
+                bars.Add(go);
+            }
+            currentOffset.x = 0;
+            currentOffset.z += 2;
+        }
+    }
+
+       
+
+   
+           
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
