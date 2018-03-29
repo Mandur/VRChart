553f4eed05134fd5d7ee9bf9cd511f3c99a95539
Mikhail Chatillon <mikhail.chatillon@hotmail.com>
2018-03-29 10:23:48 +0200

    tested with controlelr

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class ControllerScript : MonoBehaviour {
+     
+
+	// Use this for initialization
+	void Start () {
+        InteractionManager.InteractionSourcePressed += InteractionSourcePressed;
+        InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
+    }
+
+    private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs obj)
+    {
+       Debug.Log("asdsad");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
+        foreach (var interactionSourceState in interactionSourceStates)
+        {
+            Debug.Log(interactionSourceState.anyPressed);
+        }
+    }
+    private void InteractionSourcePressed(InteractionSourcePressedEventArgs obj)
+    {
+        Debug.Log("here");
+    }
+
+}
--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class CubeScript : MonoBehaviour {
     public GameObject text;
+    public Toggle Toggle;
 
 	// Use this for initialization
 	void Start () {
@@ -17,12 +19,13 @@ public class CubeScript : MonoBehaviour {
 
     public void populateCube(Dictionary<string, Material> materialList)
     {
+
         float offset = 0;
 
         foreach(var key in materialList.Keys)
         {
             Debug.Log(key);
-            var gameObject=Instantiate(text,new Vector3(-10,offset,-10),Quaternion.Euler(0,180,0));
+            var gameObject=Instantiate(text,new Vector3(-10,offset,-10),Quaternion.Euler(0,0,0));
             offset -= 1.5f;
             gameObject.GetComponent<TextMesh>().text= key;
             gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().material = materialList[key];
